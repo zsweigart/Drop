5bcd7fecd61c24af8558799bb08c7c493c6f2971
Brandon Gannicott <brandon.gannicott@gmail.com>
2014-03-26 00:31:28 -0400

    Initial Settings class set up, dummy preferences. Integer file in /res/values to store integers and integer arrays. Attempting to implement a single activity that shows a unified options menu. Getting 4.4 errors

--- Drop/src/com/example/drop/Settings.java
+++ Drop/src/com/example/drop/Settings.java
@@ -1,20 +1,31 @@
 package com.example.drop;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.app.Activity;
-import android.view.Menu;
+
 
 public class Settings extends Activity {
 
+	public static class SettingsFragment extends PreferenceFragment{
+		@Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+
+	        // Load the preferences from an XML resource
+	        addPreferencesFromResource(R.xml.preferences);
+	    }		
+		
+	}
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        
+     // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_settings, menu);
-        return true;
-    }
+  
 }
--- Drop/src/com/example/drop/SplashScreen.java
+++ Drop/src/com/example/drop/SplashScreen.java
@@ -1,15 +1,18 @@
 package com.example.drop;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.view.Menu;
 
-public class SplashScreen extends Activity {
+public class SplashScreen extends OptionsMenuScreen {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //Set default preferences (won't override user preferences)
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 
     @Override
