de4282cbe7f205da784874c6bfec336ff61c634f
Yury Chernushenko <chernushenko@Yurys-MacBook-Pro.local>
2014-04-06 15:57:14 -0400

    Map Screen Created

--- Drop/src/com/example/drop/MapScreen.java
+++ Drop/src/com/example/drop/MapScreen.java
@@ -1,20 +1,101 @@
 package com.example.drop;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.widget.Toast;
 
 public class MapScreen extends Activity {
+	
+	// Google Map
+    private GoogleMap googleMap;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_screen);
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_map_screen);
+            
+            
+            try {
+                // Loading map
+                initilizeMap();
+     
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+         
+    }
+    
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+            
+            googleMap.setMyLocationEnabled(true);
+            
+            // latitude and longitude
+            double latitude = 40.442492;
+            double longitude = -79.942553;
+             
+            // create marker
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
+             
+            // adding marker
+            googleMap.addMarker(marker);
+            
+            
+            // Acquire a reference to the system Location Manager
+            LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+            // Define a listener that responds to location updates
+            LocationListener locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                  // Called when a new location is found by the network location provider.                	
+                	double latitude= location.getLatitude();
+                	double longtitude = location.getLongitude();
+                	LatLng ll = new LatLng(latitude, longtitude);
+
+                	googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 14));
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+                public void onProviderEnabled(String provider) {}
+
+                public void onProviderDisabled(String provider) {}
+
+              };
+
+            // Register the listener with the Location Manager to receive location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);        
+
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_map_screen, menu);
-        return true;
-    }
 }
