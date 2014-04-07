59a083bdeab425c948efa77f12745422ba39f6d8
Zachary Sweigart <zsweigart@gmail.com>
2014-04-07 10:02:43 -0400

    Updated loging in through parse

--- Drop/src/com/example/drop/LoginScreen.java
+++ Drop/src/com/example/drop/LoginScreen.java
@@ -1,7 +1,11 @@
 package com.example.drop;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,11 +18,17 @@ import android.widget.TextView;
 
 import com.facebook.*;
 import com.facebook.model.*;
+import com.parse.LogInCallback;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.parse.ParseException;
 
 
 public class LoginScreen extends Activity {
 
+	protected static final String TAG = null;
 	private SharedPreferences prefs;
+	ProgressDialog progressDialog;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,7 @@ public class LoginScreen extends Activity {
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         
         // start Facebook Login
-        Session.openActiveSession(this, true, new Session.StatusCallback() {
+        /*Session.openActiveSession(this, true, new Session.StatusCallback() {
 
             // callback when session changes state
 			public void call(Session session, SessionState state, Exception exception) {
@@ -49,12 +59,13 @@ public class LoginScreen extends Activity {
                 });
               }
             }
-          });
+          });*/
         
-        Button nextButton = (Button)findViewById(R.id.loginscreen_button_login);
-        nextButton.setOnClickListener(new OnClickListener() {
+        Button loginButton = (Button)findViewById(R.id.loginscreen_button_login);
+        loginButton.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View arg0) {
+				onLoginButtonClicked();
 				Intent i = new Intent(LoginScreen.this, CameraScreen.class);
                 startActivity(i);
                 
@@ -69,9 +80,32 @@ public class LoginScreen extends Activity {
         });
     }
     
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    /*public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }*/
+    
+    private void onLoginButtonClicked() {
+        LoginScreen.this.progressDialog = ProgressDialog.show(
+        		LoginScreen.this, "", "Logging in...", true);
+        List<String> permissions = Arrays.asList("basic_info", "user_about_me",
+                "user_relationships", "user_birthday", "user_location");
+        ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException err) {
+            	LoginScreen.this.progressDialog.dismiss();
+                if (user == null) {
+                    Log.d(TAG,
+                            "Uh oh. The user cancelled the Facebook login.");
+                } else if (user.isNew()) {
+                    Log.d(TAG,
+                            "User signed up and logged in through Facebook!");
+                } else {
+                    Log.d(TAG,
+                            "User logged in through Facebook!");
+                }
+            }
+        });
     }
     
 }
--- Drop/src/com/example/drop/SplashScreen.java
+++ Drop/src/com/example/drop/SplashScreen.java
@@ -1,5 +1,8 @@
 package com.example.drop;
 
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,13 +14,16 @@ import android.preference.PreferenceManager;
 public class SplashScreen extends Activity {
 	boolean loggedIn;
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 1500;
  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        
+        Parse.initialize(this, "zdWe11jTmmRmb5wCR9dtQdF3GohrPyHeuLAzihnP", "VB2WIVxrbuzlY555vJ8Dt4LUp5s2y1vG7NCB71vU");
+        ParseAnalytics.trackAppOpened(getIntent());
  
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		//Set default preferences (won't override user preferences)
