ac4e3e8a6bba326d28abaa9fbf3fdaa09088beb3
Zachary Sweigart <zsweigart@gmail.com>
2014-05-04 16:10:25 -0400

    updated camera screen

--- Drop/src/com/example/drop/MainActivity.java
+++ Drop/src/com/example/drop/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.drop;
 
 import java.io.File;
 
-import org.json.JSONException;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -29,7 +27,6 @@ public class MainActivity extends DrawerFragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       
         
         View layout = getLayoutInflater().inflate(
 				R.layout.activity_main, null);
